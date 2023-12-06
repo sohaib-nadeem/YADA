500dec8ea840d673bffdb8accb83872047d981be
Sirius-H <y45hou@uwaterloo.ca>
2023-12-05 22:38:01 -0500

    Added help button & instructions page

--- app/src/main/java/ca/uwaterloo/cs346project/ui/home/HomeScreen.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package ca.uwaterloo.cs346project.ui.home
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Configuration
 import android.widget.Toast
@@ -18,10 +19,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -50,6 +54,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import androidx.compose.ui.res.painterResource
+import ca.uwaterloo.cs346project.R
+import ca.uwaterloo.cs346project.ui.util.openPdfFromAssets
 
 fun makeToast(context: Context, text: String){
     Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
@@ -70,153 +77,78 @@ fun DynamicUI(setPage: (PageType) -> Unit) {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PortraitUI(setPage: (PageType) -> Unit) {
-    var joinSessionID by remember { mutableStateOf("") }
-    val context = LocalContext.current
-    var loading by remember { mutableStateOf(false) }
-    ConstraintLayout(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(16.dp)
-    ) {
-        val (titleRef, buttonBoxRef) = createRefs()
+    @SuppressLint("RememberReturnType")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun PortraitUI(setPage: (PageType) -> Unit) {
+        var joinSessionID by remember { mutableStateOf("") }
+        val context = LocalContext.current
+        var loading by remember { mutableStateOf(false) }
 
-        Text(
-            text = "YADA",
-            fontSize = 50.sp,
-            fontWeight = FontWeight.Bold,
-            fontStyle = FontStyle.Italic,
-            lineHeight = 50.sp,
-            modifier = Modifier.constrainAs(titleRef) {
-                top.linkTo(parent.top, margin = 50.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            },
-            textAlign = TextAlign.Center // Center align text
-        )
-
-        BoxWithConstraints(
+        ConstraintLayout(
             modifier = Modifier
-                .constrainAs(buttonBoxRef) {
-                    top.linkTo(titleRef.bottom)
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(16.dp)
+        ) {
+            val (titleRef, buttonBoxRef) = createRefs()
+    
+            Text(
+                text = "YADA",
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                lineHeight = 50.sp,
+                modifier = Modifier.constrainAs(titleRef) {
+                    top.linkTo(parent.top, margin = 50.dp)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                    width = Dimension.fillToConstraints
-                    height = Dimension.value(500.dp)
-                }
-        ) {
-            Column(
+                },
+                textAlign = TextAlign.Center // Center align text
+            )
+    
+            BoxWithConstraints(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .padding(50.dp, 0.dp),
-                verticalArrangement = Arrangement.spacedBy(30.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .constrainAs(buttonBoxRef) {
+                        top.linkTo(titleRef.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                        width = Dimension.fillToConstraints
+                        height = Dimension.value(500.dp)
+                    }
             ) {
-                Button(
-                    onClick = {
-                        setPage(PageType.WhiteboardPage)
-                        offline = true
-                    },
+                Column(
                     modifier = Modifier
-                        .width(250.dp)
-                        .height(50.dp)
+                        .fillMaxHeight()
+                        .padding(50.dp, 0.dp),
+                    verticalArrangement = Arrangement.spacedBy(30.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text("Offline Mode", fontSize = 20.sp)
-                }
-
-                OutlinedButton(
-                    onClick = {
-                        loading = true
-                        GlobalScope.launch() {
-                            withContext(Dispatchers.IO) {
-                                if (client.create()) {
-                                    loading = false
-                                    offline = false
-                                    setPage(PageType.WhiteboardPage)
-                                } else {
-                                    loading = false
-                                    val text = "Failed to create"
-                                    withContext(Dispatchers.Main) {
-                                        makeToast(context, text)
-                                    }
-                                }
-                            }
-                        }
-                    },
-                    modifier = Modifier
-                        .width(250.dp)
-                        .height(50.dp)
-                ) {
-                    Text("New Canvas", fontSize = 20.sp)
-                }
-
-                Button(
-                    onClick = {
-                        loading = true
-                        GlobalScope.launch() {
-                            withContext(Dispatchers.IO) {
-                                if (client.join(client.session_id.toString())) {
-                                    loading = false
-                                    offline = false
-                                    setPage(PageType.WhiteboardPage)
-                                } else {
-                                    loading = false
-                                    val text = "Failed to open previous canvas"
-                                    withContext(Dispatchers.Main) {
-                                        makeToast(context, text)
-                                    }
-                                }
-                            }
-                        }
-                    },
-                    modifier = Modifier
-                        .width(250.dp)
-                        .height(50.dp)
-                ) {
-                    Text("Open Previous", fontSize = 20.sp)
-                }
-
-                Row(
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
-                    OutlinedTextField(
-                        value = joinSessionID,
-                        onValueChange = { joinSessionID = it },
-                        label = { Text("Session ID") },
-                        modifier = Modifier
-                            .width(130.dp)
-                            .height(60.dp),
-                        singleLine = true,
-                        shape = RoundedCornerShape(30.dp),
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            unfocusedBorderColor = MaterialTheme.colorScheme.onSurface,
-                            focusedBorderColor = MaterialTheme.colorScheme.primary,
-                            cursorColor = MaterialTheme.colorScheme.primary,
-                            textColor = MaterialTheme.colorScheme.onSurface,
-                            placeholderColor = MaterialTheme.colorScheme.onSurface
-                        )
-                    )
-
-
                     Button(
+                        onClick = {
+                            setPage(PageType.WhiteboardPage)
+                            offline = true
+                        },
+                        modifier = Modifier
+                            .width(250.dp)
+                            .height(50.dp)
+                    ) {
+                        Text("Offline Mode", fontSize = 20.sp)
+                    }
+    
+                    OutlinedButton(
                         onClick = {
                             loading = true
                             GlobalScope.launch() {
                                 withContext(Dispatchers.IO) {
-                                    if (client.join(joinSessionID)) {
+                                    if (client.create()) {
                                         loading = false
                                         offline = false
                                         setPage(PageType.WhiteboardPage)
                                     } else {
                                         loading = false
-                                        val text = "Invalid Session ID"
+                                        val text = "Failed to create"
                                         withContext(Dispatchers.Main) {
                                             makeToast(context, text)
                                         }
@@ -225,23 +157,112 @@ fun PortraitUI(setPage: (PageType) -> Unit) {
                             }
                         },
                         modifier = Modifier
-                            .width(100.dp)
-                            .height(60.dp)
+                            .width(250.dp)
+                            .height(50.dp)
                     ) {
-                        Text("Join", fontSize = 20.sp)
+                        Text("New Canvas", fontSize = 20.sp)
+                    }
+    
+                    Button(
+                        onClick = {
+                            loading = true
+                            GlobalScope.launch() {
+                                withContext(Dispatchers.IO) {
+                                    if (client.join(client.session_id.toString())) {
+                                        loading = false
+                                        offline = false
+                                        setPage(PageType.WhiteboardPage)
+                                    } else {
+                                        loading = false
+                                        val text = "Failed to open previous canvas"
+                                        withContext(Dispatchers.Main) {
+                                            makeToast(context, text)
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            .width(250.dp)
+                            .height(50.dp)
+                    ) {
+                        Text("Open Previous", fontSize = 20.sp)
+                    }
+    
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = joinSessionID,
+                            onValueChange = { joinSessionID = it },
+                            label = { Text("Session ID") },
+                            modifier = Modifier
+                                .width(130.dp)
+                                .height(60.dp),
+                            singleLine = true,
+                            shape = RoundedCornerShape(30.dp),
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface,
+                                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                                cursorColor = MaterialTheme.colorScheme.primary,
+                                textColor = MaterialTheme.colorScheme.onSurface,
+                                placeholderColor = MaterialTheme.colorScheme.onSurface
+                            )
+                        )
+    
+    
+                        Button(
+                            onClick = {
+                                loading = true
+                                GlobalScope.launch() {
+                                    withContext(Dispatchers.IO) {
+                                        if (client.join(joinSessionID)) {
+                                            loading = false
+                                            offline = false
+                                            setPage(PageType.WhiteboardPage)
+                                        } else {
+                                            loading = false
+                                            val text = "Invalid Session ID"
+                                            withContext(Dispatchers.Main) {
+                                                makeToast(context, text)
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                            modifier = Modifier
+                                .width(100.dp)
+                                .height(60.dp)
+                        ) {
+                            Text("Join", fontSize = 20.sp)
+                        }
                     }
                 }
+                if (loading) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.width(64.dp).align(Alignment.Center)
+                            .offset(y = (-30).dp),
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
             }
-            if (loading) {
-                CircularProgressIndicator(
-                    modifier = Modifier.width(64.dp).align(Alignment.Center)
-                        .offset(y = (-30).dp),
-                    color = MaterialTheme.colorScheme.secondary
+
+            IconButton(onClick = {
+                openPdfFromAssets(context, "instructions.pdf")
+            }) {
+                Icon(painterResource(id = R.drawable.help_24px), contentDescription = "Help", modifier = Modifier
+                    .background(
+                        color = Color.White,
+                        shape = CircleShape,
+                    )
+                    .padding(5.dp)
                 )
             }
         }
     }
-}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LandscapeUI(setPage: (PageType) -> Unit) {
@@ -399,6 +420,18 @@ fun LandscapeUI(setPage: (PageType) -> Unit) {
                 modifier = Modifier.width(64.dp).align(Alignment.Center)
                     .offset(y = (-30).dp),
                 color = MaterialTheme.colorScheme.secondary
+            )
+        }
+
+        IconButton(onClick = {
+            openPdfFromAssets(context, "instructions.pdf")
+        }) {
+            Icon(painterResource(id = R.drawable.help_24px), contentDescription = "Help", modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = CircleShape,
+                )
+                .padding(5.dp)
             )
         }
 
